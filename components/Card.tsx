79d373d84dd5e46554e8bab73d56076acecc5a9a
Daniel12346 <danezoki2@gmail.com>
2023-08-21 22:42:42 +0200

    extracted Card component to a separate file

---
+++ components/Card.tsx
@@ -0,0 +1,31 @@
+import { gameChannelState, myIDState } from "@/recoil/atoms";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { RealtimeChannel } from "@supabase/supabase-js";
+import {
+  ReactEventHandler,
+  useEffect,
+  useState,
+} from "react";
+
+interface Props {
+  card: Card;
+  handleClick?: ReactEventHandler<HTMLDivElement>;
+}
+export default function Card({ card, handleClick }: Props) {
+  return (
+    <div
+      className="bg-blue-200 flex flex-col items-center w-28"
+      onClick={handleClick}
+    >
+      <span className="h-16">{card.name}</span>
+      {/* //TODO: use Next.js Image component */}
+      <img
+        src={card.img_src}
+        className="w-full h-12 object-cover object-center"
+      />
+      <span>{card.card_type}</span>
+      <span>{card.faction}</span>
+      <span>{card.element}</span>
+    </div>
+  );
+}
--- components/DeckDisplay.tsx
+++ components/DeckDisplay.tsx
@@ -1,27 +1,9 @@
 import { myInGameCardsState, opponentInGameCardsState } from "@/recoil/atoms";
 import { useEffect } from "react";
 import { useRecoilValue } from "recoil";
+import Card from "@/components/Card";
 
-interface Props {
-  card: Card;
-}
-const Card = ({ card }: Props) => {
-  return (
-    <div className="bg-blue-200 flex flex-col items-center w-28">
-      <span className="h-16">{card.name}</span>
-      {/* //TODO: use Next.js Image component */}
-      <img
-        src={card.img_src}
-        className="w-full h-12 object-cover object-center"
-      />
-      <span>{card.card_type}</span>
-      <span>{card.faction}</span>
-      <span>{card.element}</span>
-    </div>
-  );
-};
-
-export default function GameBoard() {
+export default function DeckDisplay() {
   const myCards = useRecoilValue(myInGameCardsState);
   const opponentCards = useRecoilValue(opponentInGameCardsState);
   return (
