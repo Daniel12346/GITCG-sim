c2b0674cb5291c2188d2f3378d98ddb615e869cc
Daniel12346 <danezoki2@gmail.com>
2023-08-27 23:15:26 +0200

    added styling  to highlight selected cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,18 +1,33 @@
-import { gameChannelState, myIDState } from "@/recoil/atoms";
+import {
+  amSelectingTargetsState,
+  mySelectedTargetCardsState,
+  requiredTargetsState,
+} from "@/recoil/atoms";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { RealtimeChannel } from "@supabase/supabase-js";
 import { ReactEventHandler, useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
 
 interface Props {
   card: CardExt;
   handleClick?: ReactEventHandler<HTMLDivElement>;
 }
 export default function Card({ card, handleClick }: Props) {
+  const [amSelectingTargets, setAmSelectingTargets] = useRecoilState(
+    amSelectingTargetsState
+  );
+  const [selectedTargets, setSelectedTargets] = useRecoilState(
+    mySelectedTargetCardsState
+  );
   return (
     <div
-      className="bg-blue-200 flex flex-col items-center w-28"
+      className={`bg-blue-200 flex flex-col items-center w-28 ${
+        amSelectingTargets &&
+        selectedTargets.find((target) => target.id === card.id) &&
+        "border-4 border-green-500 border-solid"
+      }`}
       onClick={handleClick}
-      onMouseEnter={() => console.log(card.effects)}
+      onMouseEnter={() => console.log(card)}
     >
       <span className="h-16">{card.name}</span>
       {/* //TODO: use Next.js Image component */}
