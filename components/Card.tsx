00185531a7cbaf62aa4b46d4f6dd2b09002a3fdb
Daniel12346 <danezoki2@gmail.com>
2023-09-04 02:35:14 +0200

    removed info that was shown on hover because now it can be seen ic card display component, other minor fixes

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,69 +1,64 @@
 import {
   amSelectingTargetsState,
+  currentViewedCardState,
   mySelectedTargetCardsState,
   requiredTargetsState,
 } from "@/recoil/atoms";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { RealtimeChannel } from "@supabase/supabase-js";
 import { ReactEventHandler, useEffect, useState } from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
 interface Props {
   card: CardExt;
   handleClick?: ReactEventHandler<HTMLDivElement>;
   //TODO: fix
   isFaceDown?: boolean;
+  isInDeckDisplay?: boolean;
 }
-export default function Card({ card, handleClick, isFaceDown }: Props) {
+export default function Card({
+  card,
+  handleClick,
+  isFaceDown,
+  isInDeckDisplay,
+}: Props) {
   const [amSelectingTargets, setAmSelectingTargets] = useRecoilState(
     amSelectingTargetsState
   );
   const [selectedTargets, setSelectedTargets] = useRecoilState(
     mySelectedTargetCardsState
   );
+  const setCurrentViewedCard = useSetRecoilState(currentViewedCardState);
   return (
     <>
       <div
-        className={`group bg-blue-200 flex flex-col items-center relative h-24 w-16 border-4 border-orange-300 rounded-md
+        className={`group bg-blue-200 flex flex-col items-center relative h-24 w-16 border-4
+         border-orange-300 rounded-md transition-transform duration-300 ease-in-out
         ${
           amSelectingTargets &&
           selectedTargets.find((target) => target.id === card.id) &&
           "border-4 border-green-500 border-solid"
-        }`}
-        onClick={handleClick}
-        //TODO: remove
-        onMouseEnter={() => console.log(card)}
+        }
+        ${card && card.is_active && "scale-125"}
+        `}
+        onClick={(e) => {
+          handleClick && handleClick(e);
+        }}
+        onMouseEnter={() => setCurrentViewedCard(card)}
       >
         <>
+          <div className="z-10 flex justify-between w-full ml-[-1rem] mt-[-0.5rem]">
+            <span className="bg-orange-300 rounded-sm text-orange-800">
+              {isInDeckDisplay ? card.base_health : card.health}
+            </span>
+            {/* //TODO: display dice cost and energy */}
+            <span className="bg-orange-300 rounded-sm text-orange-800">{}</span>
+          </div>
           {/* //TODO: use Next.js Image component */}
           <img
             src={!isFaceDown ? card.img_src : "../card_back_origin.webp"}
-            className={`w-full absolute h-full object-cover object-center`}
+            className={`w-full absolute h-full object-cover object-center rounded-md`}
           />
-          <div className="relative hidden group-hover:flex top-[50%] left-[90%] bg-blue-300 bg-opacity-90 p-1 w-44 z-10  flex-col">
-            <span className="font-bold mb-2">{card.name}</span>
-
-            {card &&
-              card.effects &&
-              card.effects.map((effect) => {
-                return (
-                  <div className="mb-3">
-                    <p className="w-full">{effect.description}</p>
-                    <div className="flex gap-1">
-                      {effect.cost &&
-                        Object.entries(effect.cost)
-                          .sort()
-                          .map(([element, amount]) => (
-                            //TODO: color according to element
-                            <span key={card.id + element + amount.toString()}>
-                              {element}:{amount}
-                            </span>
-                          ))}
-                    </div>
-                  </div>
-                );
-              })}
-          </div>
         </>
 
         {/* //TODO: display equipped cards */}
