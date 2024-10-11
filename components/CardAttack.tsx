2d247be6537f0c366f6123f01db0402ca12e96c4
Daniel12346 <danezoki2@gmail.com>
2024-10-11 22:43:16 +0200

    added attack type name display, styling

--- components/CardAttack.tsx
+++ components/CardAttack.tsx
@@ -1,3 +1,6 @@
+import { myInGameCardsState } from "@/recoil/atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+
 type Props = {
   attack: Effect;
   playerID: string;
@@ -9,14 +12,47 @@ export default function CardAttackInfo({
   playerID,
   handleAttack,
 }: Props) {
+  const myCards = useRecoilValue(myInGameCardsState);
+  const thisCard = myCards.find((card) => card.id === attack.card_id);
+  const isCardFrozen = thisCard?.statuses?.find(
+    (status) => status.name === "FROZEN"
+  );
+  const attacktypeDisplayText = {
+    NORMAL_ATTACK: "Normal",
+    ELEMENTAL_SKILL: "Skill",
+    ELEMENTAL_BURST: "Burst",
+  };
+  // TODO: calculate and display actual cost?
   return (
-    <div className="bg-blue-300 cursor-pointer z-20" onClick={handleAttack}>
-      {/* <p className="text-xs">{attack.description}</p> */}
-      <div className="h-full">
+    <div
+      className={`flex bg-blue-300
+         ${!isCardFrozen && "cursor-pointer"} z-20`}
+      onClick={() => {
+        if (!isCardFrozen) {
+          handleAttack();
+        }
+      }}
+    >
+      {/* <p className="text-xs">{attack.name}</p> */}
+      <div
+        className={`flex flex-col items-center p-2 bg-orange-200  ${
+          isCardFrozen && "bg-gray-700 opacity-60"
+        }`}
+      >
+        <span className="text-lg mb-1">
+          {attack?.effectType &&
+            attacktypeDisplayText[
+              //TODO: is it always one of these 3?
+              attack.effectType as
+                | "NORMAL_ATTACK"
+                | "ELEMENTAL_SKILL"
+                | "ELEMENTAL_BURST"
+            ]}
+        </span>
         {Object.entries(attack.cost!)
           .sort()
           .map(([element, amount]) => (
-            <div key={element + playerID}>
+            <div key={element + amount + playerID}>
               <span>
                 {element}:{amount}
               </span>
