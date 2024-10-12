1a9e5c196e1a057d57d2d2dda6229cc753c3bb3b
Daniel12346 <danezoki2@gmail.com>
2024-10-12 22:15:53 +0200

    Refactor CardAttack component and improve attack display

--- components/CardAttack.tsx
+++ components/CardAttack.tsx
@@ -1,4 +1,8 @@
-import { myInGameCardsState } from "@/recoil/atoms";
+import {
+  currentPhaseState,
+  isMyTurnState,
+  myInGameCardsState,
+} from "@/recoil/atoms";
 import { useRecoilState, useRecoilValue } from "recoil";
 
 type Props = {
@@ -22,43 +26,48 @@ export default function CardAttackInfo({
     ELEMENTAL_SKILL: "Skill",
     ELEMENTAL_BURST: "Burst",
   };
+  const isMyTurn = useRecoilValue(isMyTurnState);
+  const currentPhase = useRecoilValue(currentPhaseState);
   // TODO: calculate and display actual cost?
   return (
-    <div
-      className={`flex bg-blue-300
-         ${!isCardFrozen && "cursor-pointer"} z-20`}
-      onClick={() => {
-        if (!isCardFrozen) {
-          handleAttack();
-        }
-      }}
-    >
-      {/* <p className="text-xs">{attack.name}</p> */}
+    currentPhase === "ACTION_PHASE" && (
       <div
-        className={`flex flex-col items-center p-2 bg-orange-200  ${
-          isCardFrozen && "bg-gray-700 opacity-60"
-        }`}
+        className={`flex bg-blue-300
+         ${!isCardFrozen && "cursor-pointer"} z-20`}
+        onClick={() => {
+          if (!isCardFrozen) {
+            handleAttack();
+          }
+        }}
       >
-        <span className="text-lg mb-1">
-          {attack?.effectType &&
-            attacktypeDisplayText[
-              //TODO: is it always one of these 3?
-              attack.effectType as
-                | "NORMAL_ATTACK"
-                | "ELEMENTAL_SKILL"
-                | "ELEMENTAL_BURST"
-            ]}
-        </span>
-        {Object.entries(attack.cost!)
-          .sort()
-          .map(([element, amount]) => (
-            <div key={element + amount + playerID}>
-              <span>
-                {element}:{amount}
-              </span>
-            </div>
-          ))}
+        {/* <p className="text-xs">{attack.name}</p> */}
+        <div
+          className={`flex flex-col items-center p-2 bg-orange-200 
+             ${!isMyTurn && "opacity-60"}
+            ${isCardFrozen && "bg-gray-600"}
+          }`}
+        >
+          <span className="text-lg mb-1">
+            {attack?.effectType &&
+              attacktypeDisplayText[
+                //TODO: is it always one of these 3?
+                attack.effectType as
+                  | "NORMAL_ATTACK"
+                  | "ELEMENTAL_SKILL"
+                  | "ELEMENTAL_BURST"
+              ]}
+          </span>
+          {Object.entries(attack.cost!)
+            .sort()
+            .map(([element, amount]) => (
+              <div key={element + amount + playerID}>
+                <span>
+                  {element}:{amount}
+                </span>
+              </div>
+            ))}
+        </div>
       </div>
-    </div>
+    )
   );
 }
