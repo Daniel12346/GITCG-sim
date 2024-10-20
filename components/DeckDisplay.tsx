03efdaa1d9aa044513258c18063970a662ee2851
Daniel12346 <danezoki2@gmail.com>
2024-10-20 21:33:07 +0200

    Refactor DeckDisplay component to use deckInDeckBuilder state for improved performance

--- components/DeckDisplay.tsx
+++ components/DeckDisplay.tsx
@@ -1,31 +1,85 @@
-import { myCurrentDeckCardsBasicInfoState } from "@/recoil/atoms";
+import {
+  deckInDeckBuilderCardsState,
+  deckInDeckBuilderNameState,
+  myCurrentDeckCardsBasicInfoWithQuantitiesAndEffectsState,
+  deckInDeckBuilderCardsBasicInfoWithQuantitiesAndEffectsState,
+  myDecksState,
+  CardBasicInfoWithQuantityAndEffects,
+  myCurrentDeckWithCardBasicInfoState,
+} from "@/recoil/atoms";
 import { useEffect } from "react";
-import { useRecoilValue } from "recoil";
-import { cardFromBasicInfo } from "@/app/utils";
+import {
+  useRecoilState,
+  useRecoilValue,
+  useRecoilValueLoadable,
+  useSetRecoilState,
+} from "recoil";
 import CardInDeckDisplay from "./CardInDeckDisplay";
+import SetCurrentDeck from "./SelectCurrentDeck";
 
 export default function DeckDisplay() {
   //TODO: view other players' decks
+  const myDecks = useRecoilValue(myDecksState);
+  const myCurrentDeckLoadable = useRecoilValueLoadable(
+    myCurrentDeckWithCardBasicInfoState
+  );
+  const myCurrentDeckCardsLoadable = useRecoilValueLoadable(
+    myCurrentDeckCardsBasicInfoWithQuantitiesAndEffectsState
+  );
+  const setDeckInDeckBuilderName = useSetRecoilState(
+    deckInDeckBuilderNameState
+  );
+  const [
+    deckInDeckBuilderCardsBasicInfoWithQuantitiesAndEffects,
+    setDeckInDeckBuilderCardsBasicInfoWithQuantitiesAndEffects,
+  ] = useRecoilState(
+    deckInDeckBuilderCardsBasicInfoWithQuantitiesAndEffectsState
+  );
 
-  const myDeckCardsBasicInfo = useRecoilValue(myCurrentDeckCardsBasicInfoState);
-  const myDeckCards = myDeckCardsBasicInfo?.map((card) => {
-    return cardFromBasicInfo(card);
-  });
   useEffect(() => {
-    console.log("myDeckCardsBasicInfo", myDeckCardsBasicInfo);
-    // const
-  }, [myDeckCardsBasicInfo]);
+    myCurrentDeckCardsLoadable.state === "hasValue" &&
+      myCurrentDeckCardsLoadable.contents &&
+      setDeckInDeckBuilderCardsBasicInfoWithQuantitiesAndEffects(
+        myCurrentDeckCardsLoadable.contents
+      );
+    console.log(myCurrentDeckCardsLoadable.contents);
+  }, [myCurrentDeckCardsLoadable]);
+  useEffect(() => {
+    myCurrentDeckLoadable &&
+      myCurrentDeckCardsLoadable.state === "hasValue" &&
+      setDeckInDeckBuilderName(myCurrentDeckLoadable.contents?.name ?? "");
+  }, [myCurrentDeckLoadable]);
 
-  const sortedDeckCards = myDeckCards?.sort((a, b) => {
-    if (a.card_type === "CHARACTER" && b.card_type !== "CHARACTER") return -1;
-    if (b.card_type === "CHARACTER" && a.card_type !== "CHARACTER") return 1;
-    return 0;
-  });
+  const sortDeckCards = (cards: CardBasicInfoWithQuantityAndEffects[]) =>
+    cards?.toSorted((a, b) => {
+      if (a.card_type === "CHARACTER" && b.card_type !== "CHARACTER") return -1;
+      if (b.card_type === "CHARACTER" && a.card_type !== "CHARACTER") return 1;
+      return 0;
+    });
+
   return (
-    <div className="flex bg-slate-600 flex-row gap-4 justify-evenly flex-wrap rounded-md p-4">
-      {sortedDeckCards?.map((card) => {
-        return <CardInDeckDisplay key={card.id} card={card} isInDeck={true} />;
-      })}
-    </div>
+    <>
+      <ul>
+        {myDecks?.map((deck) => {
+          return <SetCurrentDeck key={deck.id} deck={deck} />;
+        })}
+      </ul>
+      {myCurrentDeckCardsLoadable.state === "loading" && (
+        <div className="text-slate-300">
+          <div className="flex justify-center w-full"></div>
+          <span>Loading deck....</span>
+        </div>
+      )}
+
+      <div className="flex bg-slate-600 flex-row gap-4 justify-evenly flex-wrap rounded-md p-4">
+        {sortDeckCards(
+          deckInDeckBuilderCardsBasicInfoWithQuantitiesAndEffects
+        ).map((card) => {
+          return (
+            <CardInDeckDisplay key={card.id} card={card} isInDeck={true} />
+          );
+        })}
+      </div>
+    </>
   );
 }
