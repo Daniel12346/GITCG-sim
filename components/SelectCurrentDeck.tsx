ad8b7553ca20d4e845e16fb885022325515a1cc9
Daniel12346 <danezoki2@gmail.com>
2024-10-23 20:41:43 +0200

    added deck name, removed unused code

--- components/SelectCurrentDeck.tsx
+++ components/SelectCurrentDeck.tsx
@@ -1,15 +1,20 @@
 "use client";
 import { Tables } from "@/lib/database.types";
-import { myCurrentDeckIDState } from "@/recoil/atoms";
+import {
+  deckInDeckBuilderNameState,
+  myCurrentDeckIDState,
+} from "@/recoil/atoms";
 import { useRouter } from "next/navigation";
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
 interface Props {
   deck: Tables<"deck">;
 }
 export default function SetCurrentDeck({ deck }: Props) {
   const setMyCurrentDeckID = useSetRecoilState(myCurrentDeckIDState);
-  const router = useRouter();
+  const setDeckInDeckBuilderName = useSetRecoilState(
+    deckInDeckBuilderNameState
+  );
   return (
     <div className="flex gap-2 items-center mb-2">
       <span>{deck.name}</span>
@@ -17,6 +22,7 @@ export default function SetCurrentDeck({ deck }: Props) {
         className="bg-blue-500 hover:bg-blue-700 text-blue-100 font-bold p-1 rounded-lg"
         onClick={() => {
           setMyCurrentDeckID(deck.id);
+          setDeckInDeckBuilderName(deck.name ?? "");
         }}
       >
         set active
