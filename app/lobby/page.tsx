470af8ad2b325bd5788088eab316e812b694fd5e
Daniel12346 <danezoki2@gmail.com>
2024-10-26 22:30:19 +0200

    removed unused imports, styling changes

--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import MyInfo from "@/components/LobbyInfo";
 import dynamic from "next/dynamic";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
 
 export default function Lobby() {
@@ -12,25 +10,21 @@ export default function Lobby() {
   const LobbyChannelNoSSR = dynamic(() => import("@/components/LobbyChannel"), {
     ssr: false,
   });
-  const GameBoardNoSSR = dynamic(() => import("@/components/GameBoard"), {
-    ssr: false,
-  });
+
   const router = useRouter();
 
   return (
     <div className="pt-20 max-w-xl w-full">
-      <div>
-        <button
-          className=" text-orange-300 p-0.5 outline mb-4 "
-          onClick={() => {
-            router.push("/");
-          }}
-        >
-          {"<-"} cancel
-        </button>
-        <LobbyInfoNoSSR />
-        <LobbyChannelNoSSR />
-      </div>
+      <button
+        className="text-lg  text-slate-100  mb-4 text-center rounded px-2 py-1 bg-amber-800 hover:bg-amber-700"
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        {"<"} cancel
+      </button>
+      <LobbyInfoNoSSR />
+      <LobbyChannelNoSSR />
     </div>
   );
 }
