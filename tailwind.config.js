22ae2f4cc3cf34c1d65953a52f7733a350303752
Daniel12346 <danezoki2@gmail.com>
2024-10-26 22:17:10 +0200

    added more color scheme variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        fieldMain: "hsl(var(--field-background-main))",
+        fieldHand: "hsl(var(--field-background-hand))",
+        fieldSecondary: "hsl(var(--field-background-secondary))",
         btn: {
           background: "hsl(var(--btn-background))",
           "background-hover": "hsl(var(--btn-background-hover))",
