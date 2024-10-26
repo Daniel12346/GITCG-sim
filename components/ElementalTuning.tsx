bc6678a17d82ef0b0978e3292aa0c922951e6441
Daniel12346 <danezoki2@gmail.com>
2024-10-26 22:21:28 +0200

    enabled tuning multiple dices at the same time

--- components/ElementalTuning.tsx
+++ components/ElementalTuning.tsx
@@ -1,13 +1,15 @@
 import { subtractCost } from "@/app/actions";
 import { CardExtended } from "@/app/global";
+import { calculateTotalDice } from "@/app/utils";
 import {
+  errorMessageState,
   myDiceState,
   myInGameCardsState,
   mySelectedCardsState,
   mySelectedDiceState,
 } from "@/recoil/atoms";
 import { RealtimeChannel } from "@supabase/supabase-js";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 export default function ElementalTuning({
   channel,
 }: {
@@ -18,29 +20,35 @@ export default function ElementalTuning({
   const [selectedCards, setSelectedCards] =
     useRecoilState(mySelectedCardsState);
   const [selectedDice, setSelectedDice] = useRecoilState(mySelectedDiceState);
+  const setErrorMessage = useSetRecoilState(errorMessageState);
   const performElementalTuning = () => {
+    if (selectedCards.length === 0)
+      return { errorMessage: "No cards selected to discard" };
     if (!myCards) return { errorMessage: "No cards" };
     if (!myDice) return { errorMessage: "No dice" };
     //TODO: check the amount of dice
-    if (!selectedDice) return { errorMessage: "No die to tune" };
+    if (!selectedDice) return { errorMessage: "No dice to tune" };
     //TODO: discarding multiple cards
-    if (selectedCards.length > 1) return { errorMessage: "Too many cards" };
-    const cardToDiscard = selectedCards[0];
-    if (cardToDiscard.location !== "HAND") {
+    if (selectedCards.length !== calculateTotalDice(selectedDice))
+      return { errorMessage: "Number of cards selected does not match dice" };
+
+    if (selectedCards.some((card) => card.location !== "HAND")) {
       return { errorMessage: "Card not in hand" };
     }
     if (selectedDice["OMNI"]) {
-      return { errorMessage: "Cannot use OMNI die" };
+      alert("Cannot tune OMNI die");
+      return { errorMessage: "Cannot tune OMNI die" };
     }
     try {
-      console.log("TUNING", myDice, selectedDice, cardToDiscard);
       let myUpdatedDice = subtractCost(myDice, selectedDice);
       myUpdatedDice = {
         ...myUpdatedDice,
-        OMNI: myUpdatedDice.OMNI ? myUpdatedDice.OMNI + 1 : 1,
+        OMNI: myUpdatedDice.OMNI
+          ? myUpdatedDice.OMNI + selectedCards.length
+          : selectedCards.length,
       };
       const myUpdatedCards = myCards.map((card) => {
-        if (card.id === cardToDiscard.id) {
+        if (selectedCards.find((selectedCard) => selectedCard.id === card.id)) {
           return {
             ...card,
             location: "DISCARD",
@@ -60,11 +68,20 @@ export default function ElementalTuning({
     } catch (e) {
       return { errorMessage: "Not enough dice" };
     }
+    return {};
   };
   return (
-    <div className="bg-blue-400">
+    <div className="px-3">
       {/* //TODO: error handling */}
-      <button onClick={performElementalTuning}>Tune</button>
+      <button
+        className="px-1 bg-blue-800 font-semibold text-md text-blue-200 w-fit"
+        onClick={() => {
+          const { errorMessage } = performElementalTuning();
+          errorMessage && setErrorMessage(errorMessage);
+        }}
+      >
+        Tune
+      </button>
     </div>
   );
 }
