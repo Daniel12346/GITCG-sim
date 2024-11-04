9e7113239bb94b57efa265e3b62645f19688a21a
Daniel12346 <danezoki2@gmail.com>
2024-11-04 21:10:13 +0100

    used DiceDisplay for dice in effects, displayed energy in effects

--- components/CurrentViewedCard.tsx
+++ components/CurrentViewedCard.tsx
@@ -1,27 +1,22 @@
 "use client";
 
-import { DiceT } from "@/app/global";
 import { currentViewedCardState } from "@/recoil/atoms";
 import { useRecoilValue } from "recoil";
+import DiceDisplay from "./DiceDisplay";
 
 export default () => {
   const currentCard = useRecoilValue(currentViewedCardState);
-  const printCost = (cost: DiceT) =>
-    Object.entries(cost)
-      .sort()
-      .map(([element, amount], index) => (
-        //TODO: style according to element
-        <span key={element + index}>
-          {element}:{amount}
-        </span>
-      ));
-
   return (
-    <div className="w-full flex justify-center text-slate-200 position-relative">
+    <div className="w-full flex justify-center text-slate-200 relative pt-2">
       <div className="flex flex-col items-center gap-3">
         <span className="text-xl">{currentCard?.name}</span>
         <img src={currentCard?.img_src} className="h-80" />
-        <span>{currentCard?.cost && printCost(currentCard.cost)}</span>
+        {currentCard?.cost && (
+          <span>
+            <span>COST:</span>
+            <DiceDisplay dice={currentCard?.cost} isMyBoard={false} />
+          </span>
+        )}
         {currentCard?.statuses && (
           <div className="flex flex-col gap-1">
             {currentCard.statuses.map((status) => (
@@ -34,16 +29,54 @@ export default () => {
         )}
         <div className="flex flex-col gap-1 max-h-[10rem] overflow-y-scroll translate-x-0">
           {currentCard?.effects &&
-            currentCard.effects.map((effect) => {
-              return (
-                <div className="w-full p-2" key={effect.id}>
-                  <p className="w-full text-slate-200">{effect.description}</p>
-                  <div className="flex gap-[0.5rem]">
-                    {effect.cost && printCost(effect.cost)}
+            currentCard.effects
+              //TODO: fix sorting
+              ?.toSorted((a, b) => {
+                if (a.effectType === "NORMAL_ATTACK") return -1;
+                if (a.effectType === "ELEMENTAL_SKILL") return 0;
+                if (a.effectType === "ELEMENTAL_BURST") return 1;
+                return 0;
+              })
+              .map((effect) => {
+                return (
+                  <div className="w-full p-2" key={effect.id}>
+                    <p className="w-full text-slate-200">
+                      {effect.description}
+                    </p>
+                    <div className="flex gap-[0.5rem]">
+                      {effect.cost && (
+                        <div>
+                          <DiceDisplay
+                            dice={Object.fromEntries(
+                              Object.entries(effect.cost).filter(
+                                ([key]) => key != "ENERGY"
+                              )
+                            )}
+                            isMyBoard={false}
+                          />
+                          {effect.effectType === "ELEMENTAL_BURST" &&
+                            currentCard.max_energy && (
+                              <div className="flex items-center px-4">
+                                {/* <span>energy:</span> */}
+                                <ul className="flex gap-3">
+                                  {Array.from({
+                                    length: currentCard.max_energy,
+                                  }).map((_, i) => (
+                                    <img
+                                      key={"energy" + i}
+                                      src="/energy_icon.svg"
+                                      className="w-6 h-6"
+                                    />
+                                  ))}
+                                </ul>
+                              </div>
+                            )}
+                        </div>
+                      )}
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
         </div>
       </div>
     </div>
