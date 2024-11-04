2510158997816f62fda7a648de7412fdd1eee6c2
Daniel12346 <danezoki2@gmail.com>
2024-11-04 21:02:32 +0100

    refactor: use useRecoilState for current deck ID management

--- components/SelectCurrentDeck.tsx
+++ components/SelectCurrentDeck.tsx
@@ -4,18 +4,19 @@ import {
   deckInDeckBuilderNameState,
   myCurrentDeckIDState,
 } from "@/recoil/atoms";
-import { useRouter } from "next/navigation";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 
 interface Props {
   deck: Tables<"deck">;
 }
 export default function SetCurrentDeck({ deck }: Props) {
-  const setMyCurrentDeckID = useSetRecoilState(myCurrentDeckIDState);
+  const [myCurrentDeckID, setMyCurrentDeckID] =
+    useRecoilState(myCurrentDeckIDState);
   const setDeckInDeckBuilderName = useSetRecoilState(
     deckInDeckBuilderNameState
   );
-  return (
+
+  return deck.id === myCurrentDeckID ? null : (
     <div className="flex gap-2 items-center mb-2">
       <span>{deck.name}</span>
       <button
