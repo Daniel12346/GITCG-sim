c8c3b08c573b2b4ce1d9347472c689f9fd554737
Daniel12346 <danezoki2@gmail.com>
2024-11-15 23:54:16 +0100

    add PlayerBattleStats component to display player wins and losses

---
+++ components/PlayerBattleStats.tsx
@@ -0,0 +1,21 @@
+import { playerBattleLogsState, playerBattleStatsState } from "@/recoil/atoms";
+import { useRecoilValue } from "recoil";
+
+type Props = {
+  playerID: string;
+};
+export default function PlayerBattleStats({ playerID }: Props) {
+  const battleStats = useRecoilValue(playerBattleStatsState(playerID));
+  return (
+    <div className="self-center text-xl flex gap-3">
+      <span>
+        Wins:
+        <span className="ml-1 text-green-400">{battleStats?.wins}</span>
+      </span>
+      <span>
+        Losses:
+        <span className="ml-1 text-red-400">{battleStats?.losses}</span>
+      </span>
+    </div>
+  );
+}
