a9a8fb9548abf0bcece3269fdd16feb022f76a54
Daniel12346 <danezoki2@gmail.com>
2024-11-16 21:21:01 +0100

    replace username display with link to profile and add avatar image in MyID component

--- components/MyID.tsx
+++ components/MyID.tsx
@@ -2,14 +2,23 @@
 import { myProfileState, mySessionState } from "@/recoil/atoms";
 import { useRecoilValue } from "recoil";
 import LogoutButton from "./LogoutButton";
+import Link from "next/link";
 
 export default function MyID() {
   const myProfile = useRecoilValue(myProfileState);
   return (
     <div className="px-2 text-slate-200 flex items-center gap-2">
       <span className="font-thin">logged in as:</span>
-      <span className="text-lg">{" " + myProfile?.username}</span>
-      {myProfile && <LogoutButton />}
+      <Link href="/me" className="flex items-center gap-2 cursor-pointer">
+        <span>
+          <img
+            className="rounded-sm w-8 h-8 object-cover object-center"
+            src={myProfile?.avatar_url || "/card_back_origin.png"}
+          />
+        </span>
+        <span className="text-lg">{" " + myProfile?.username}</span>
+      </Link>
+      <div>{myProfile && <LogoutButton />}</div>
     </div>
   );
 }
