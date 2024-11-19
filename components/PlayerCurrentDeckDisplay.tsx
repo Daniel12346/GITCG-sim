e3d028c3b065f3eb3db61f8e5c4efd3180e11775
Daniel12346 <danezoki2@gmail.com>
2024-11-19 22:08:55 +0100

    add DeckCopyButton to PlayerCurrentDeckDisplay for optional deck copying

--- components/PlayerCurrentDeckDisplay.tsx
+++ components/PlayerCurrentDeckDisplay.tsx
@@ -1,16 +1,35 @@
-import { userCurrentDeckCardsBasicInfoWithQuantitiesAndEffectsState } from "@/recoil/atoms";
+import {
+  userCurrentDeckCardsBasicInfoWithQuantitiesAndEffectsState,
+  userProfileState,
+} from "@/recoil/atoms";
 import DeckDisplay from "./DeckDisplay";
 import { useRecoilValue } from "recoil";
+import DeckCopyButton from "./DeckCopyButton";
 
 type Props = {
   playerID: string;
+  canBeCopied?: boolean;
 };
-export default function PlayerCurrentDeckDisplay({ playerID }: Props) {
+export default function PlayerCurrentDeckDisplay({
+  playerID,
+  // playerUsername,
+  canBeCopied = true,
+}: Props) {
   const userCurrentDeckCardsWithQuantitiesAndEffects = useRecoilValue(
     userCurrentDeckCardsBasicInfoWithQuantitiesAndEffectsState(playerID)
   );
-
+  const playerProfile = useRecoilValue(userProfileState(playerID));
   return userCurrentDeckCardsWithQuantitiesAndEffects ? (
-    <DeckDisplay deckCards={userCurrentDeckCardsWithQuantitiesAndEffects} />
+    <div>
+      {canBeCopied && (
+        <div className="flex justify-end">
+          <DeckCopyButton
+            deckCards={userCurrentDeckCardsWithQuantitiesAndEffects}
+            deckName={playerProfile?.username + "'s deck"}
+          />
+        </div>
+      )}
+      <DeckDisplay deckCards={userCurrentDeckCardsWithQuantitiesAndEffects} />
+    </div>
   ) : null;
 }
