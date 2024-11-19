0af9a1faabb47b17f91e54e1ef5a0cdc0c15fcc0
Daniel12346 <danezoki2@gmail.com>
2024-11-19 22:07:51 +0100

    replace dynamic imports with MyProfile component in Me page

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,39 +1,13 @@
 "use client";
+import MyProfile from "@/components/MyProfile";
 import { myIDState } from "@/recoil/atoms";
-import dynamic from "next/dynamic";
 import { useRecoilValue } from "recoil";
-
-const DeckDisplayNoSSR = dynamic(() => import("@/components/MyDecksDisplay"), {
-  ssr: false,
-});
-// const MyDecksNoSSR = dynamic(() => import("@/components/MyDecks"), {
-//   ssr: false,
-// });
-const MyInfoNoSSR = dynamic(() => import("@/components/MyInfo"), {
-  ssr: false,
-});
-const PlayerCurrentDeckDisplayNoSSR = dynamic(
-  () => import("@/components/PlayerCurrentDeckDisplay"),
-  {
-    ssr: false,
-  }
-);
 
 export default function Me() {
   const myID = useRecoilValue(myIDState);
   return (
-    <div className="flex flex-col w-full items-center">
-      <div className="max-w-xl flex flex-col gap-4">
-        <div className="max-w-md">
-          <MyInfoNoSSR />
-        </div>
-        {/* <MyDecksNoSSR /> */}
-        <div className="flex justify-center max-w-lg">
-          <PlayerCurrentDeckDisplayNoSSR
-            playerID={myID}
-          ></PlayerCurrentDeckDisplayNoSSR>
-        </div>
-      </div>
+    <div className="flex justify-center max-w-lg mt-3">
+      <MyProfile playerID={myID} />
     </div>
   );
 }
