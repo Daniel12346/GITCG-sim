f0cf9bedbc8334463e65415de2f8eaa9d19a0314
Daniel12346 <danezoki2@gmail.com>
2024-11-24 01:20:58 +0100

    used dynamic import for MyProfile componen

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import MyProfile from "@/components/MyProfile";
-import { myIDState } from "@/recoil/atoms";
-import { useRecoilValue } from "recoil";
+import dynamic from "next/dynamic";
 
+const MyProfileNoSSR = dynamic(() => import("@/components/MyProfile"), {
+  ssr: false,
+});
 export default function Me() {
-  const myID = useRecoilValue(myIDState);
   return (
     <div className="flex justify-center max-w-lg mt-3">
-      <MyProfile playerID={myID} />
+      <MyProfileNoSSR />
     </div>
   );
 }
