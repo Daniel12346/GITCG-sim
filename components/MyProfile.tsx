ae5a5b6de1f4009aa4eeefbfc48b2b4216de5955
Daniel12346 <danezoki2@gmail.com>
2024-11-24 01:19:58 +0100

    added settings section with avatar and banner selection

--- components/MyProfile.tsx
+++ components/MyProfile.tsx
@@ -1,23 +1,26 @@
-import { myProfileState, userProfileState } from "@/recoil/atoms";
+import { myIDState, myProfileState, userProfileState } from "@/recoil/atoms";
 import { useRecoilValue } from "recoil";
 import PlayerProfileBanner from "./PlayerProfileBanner";
 import PlayerCurrentDeckDisplay from "./PlayerCurrentDeckDisplay";
 import PlayerBattleStats from "./PlayerBattleStats";
+import SelectAvatar from "./SelectAvatar";
+import SelectBanner from "./SelectBanner";
 
-type Props = {
-  playerID: string;
-};
-export default function MyProfile({ playerID }: Props) {
+export default function MyProfile() {
   const myProfile = useRecoilValue(myProfileState);
+  const myID = useRecoilValue(myIDState);
   return (
     <div className="flex flex-col">
-      {/* TODO: make profile image and banner customizable */}
-      <PlayerProfileBanner playerProfile={myProfile} />;
-      <div className="text-slate-200">
-        <PlayerBattleStats playerID={playerID} />
+      <PlayerProfileBanner isMyProfile={true} playerProfile={myProfile} />
+      <div className="flex flex-col">
+        <span className="text-blue-300 uppercase font-semibold">settings</span>
+        <SelectAvatar />
+        <SelectBanner />
       </div>
-      {/* <span className="text-blue-200">current deck</span> */}
-      <PlayerCurrentDeckDisplay playerID={playerID} canBeCopied={false} />
+      <div className="text-slate-200">
+        <PlayerBattleStats playerID={myID} />
+      </div>
+      <PlayerCurrentDeckDisplay playerID={myID} canBeCopied={false} />
     </div>
   );
 }
