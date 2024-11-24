f8174356017e44df4ffb29f65bfcd25ac8033311
Daniel12346 <danezoki2@gmail.com>
2024-11-24 01:18:29 +0100

    used userBanner and userAvatar

--- components/PlayerBannerInGame.tsx
+++ components/PlayerBannerInGame.tsx
@@ -1,5 +1,10 @@
 import { Tables } from "@/lib/database.types";
-import { isActionPhaseState, isMyTurnState } from "@/recoil/atoms";
+import {
+  isActionPhaseState,
+  isMyTurnState,
+  userAvatarState,
+  userBannerState,
+} from "@/recoil/atoms";
 import { useRecoilValue } from "recoil";
 
 type Props = {
@@ -13,10 +18,13 @@ export default function PlayerBannerInGame({
   //TODO: add loadable state
   const isMyTurn = useRecoilValue(isMyTurnState);
   const isActionPhase = useRecoilValue(isActionPhaseState);
+  const userAvatar = useRecoilValue(userAvatarState(playerProfile?.id));
+  const userBanner = useRecoilValue(userBannerState(playerProfile?.id));
   return (
     <div
-      className={` 
+      className={`
         py-2
+        relative
         ${isMyProfile ? "bottom-0 rounded-tr-lg" : "top-0 rounded-br-lg"}
         z-100
         bg-indigo-600
@@ -28,11 +36,17 @@ export default function PlayerBannerInGame({
         }
          flex flex-col  lg:flex-row lg:items-center pl-2 pr-3 gap-6 min-w-fit`}
     >
+      {userBanner && (
+        <img
+          src={userBanner}
+          className="absolute object-cover top-0 left-0 border-inherit w-full h-full bg-red-100 z-10"
+        ></img>
+      )}
       <img
-        className="rounded-sm w-14 h-14 object-cover object-center"
-        src={playerProfile?.avatar_url || "/card_back_origin.png"}
+        className="rounded-sm z-10 w-14 h-14 object-cover object-center"
+        src={userAvatar || "/card_back_origin.png"}
       />
-      <span className="font-bold text-xl text-blue-100">
+      <span className="font-bold z-10 text-xl text-blue-100">
         {playerProfile?.username}
       </span>
     </div>
