3c7ddda8c08fec8a106e5dd12aca0acb70fbe8e1
Daniel12346 <danezoki2@gmail.com>
2024-11-24 01:20:28 +0100

    used myAvatar and TextWithSlideUnderline

--- components/MyID.tsx
+++ components/MyID.tsx
@@ -1,22 +1,28 @@
 "use client";
-import { myProfileState, mySessionState } from "@/recoil/atoms";
+import { myAvatarState, myProfileState, mySessionState } from "@/recoil/atoms";
 import { useRecoilValue } from "recoil";
 import LogoutButton from "./LogoutButton";
 import Link from "next/link";
+import TextWithSlideInUnderline from "./TextWithSlideInUnderline";
 
 export default function MyID() {
   const myProfile = useRecoilValue(myProfileState);
+  const myAvatar = useRecoilValue(myAvatarState);
   return (
     <div className="px-2 text-slate-200 flex items-center gap-2">
       <span className="font-thin">logged in as:</span>
-      <Link href="/me" className="flex items-center gap-2 cursor-pointer">
+      <Link href="/me" className="flex items-center gap-2 group cursor-pointer">
         <span>
           <img
             className="rounded-sm w-8 h-8 object-cover object-center"
-            src={myProfile?.avatar_url || "/card_back_origin.png"}
+            src={myAvatar || "/card_back_origin.png"}
           />
         </span>
-        <span className="text-lg">{" " + myProfile?.username}</span>
+        <span className="text-lg">
+          <TextWithSlideInUnderline>
+            {" " + myProfile?.username}
+          </TextWithSlideInUnderline>
+        </span>
       </Link>
       <div>{myProfile && <LogoutButton />}</div>
     </div>
