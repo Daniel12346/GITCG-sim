71c700b8308ad323f50f6c5d5b7cea8fbdf5da88
Daniel12346 <danezoki2@gmail.com>
2024-11-25 01:48:33 +0100

    added edit functionality for avatar and banner

--- components/PlayerProfileBanner.tsx
+++ components/PlayerProfileBanner.tsx
@@ -1,12 +1,8 @@
-import { uploadToSupabaseBucket } from "@/app/utils";
 import { Tables } from "@/lib/database.types";
-import {
-  myAvatarState,
-  userAvatarState,
-  userBannerState,
-} from "@/recoil/atoms";
-import { useEffect } from "react";
+import { userAvatarState, userBannerState } from "@/recoil/atoms";
 import { useRecoilValue } from "recoil";
+import EditAvatar from "./EditAvatar";
+import EditBanner from "./EditBanner";
 
 type Props = {
   playerProfile: Tables<"profile"> | null;
@@ -14,14 +10,11 @@ type Props = {
 };
 export default function PlayerProfileBanner({
   playerProfile,
+  //TODO: calculate isMyProfile in component
   isMyProfile,
 }: Props) {
   const userAvatar = useRecoilValue(userAvatarState(playerProfile?.id));
-  useEffect(() => {
-    console.log("userAvatar", userAvatar, playerProfile?.id);
-  }, [userAvatar]);
   const userBanner = useRecoilValue(userBannerState(playerProfile?.id));
-  console.log("userBanner", userBanner, playerProfile?.id);
   return (
     <div
       className={` 
@@ -30,7 +23,7 @@ export default function PlayerProfileBanner({
         z-100
         bg-indigo-600
         border-4 
-         flex flex-col  lg:flex-row lg:items-center pl-2 pr-3 gap-6 min-w-fit`}
+         flex flex-col  lg:flex-row lg:items-center px-6 gap-4 min-w-fit`}
     >
       {userBanner && (
         <>
@@ -38,14 +31,24 @@ export default function PlayerProfileBanner({
             src={userBanner}
             className="absolute object-cover top-0 left-0 border-inherit w-full h-full z-10"
           ></img>
-          <div className="absolute object-cover top-0 left-0 border-inherit w-full h-full bg-gradient-to-r from-slate-800/60 to-slate-800/10 z-10"></div>
+          {isMyProfile && (
+            <div className="absolute object-cover top-0 left-0 border-inherit w-full h-full bg-gradient-to-r from-slate-800/60 to-slate-800/10 z-10">
+              <EditBanner />
+            </div>
+          )}
         </>
       )}
-
-      <img
-        className="rounded-sm w-14 h-14 object-cover z-10 object-center"
-        src={userAvatar || "/card_back_origin.png"}
-      />
+      <div className="flex relative">
+        <img
+          className="rounded-sm w-14 h-14 object-cover z-10 object-center"
+          src={userAvatar || "/card_back_origin.png"}
+        />
+        {isMyProfile && (
+          <div className="absolute top-0 right-0 w-fit h-fit z-30">
+            <EditAvatar />
+          </div>
+        )}
+      </div>
       <span className="font-bold text-xl text-blue-100 z-10">
         {playerProfile?.username}
       </span>
