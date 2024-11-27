00e0a8a0a2585adeeb563911cd3ea7074d7dc2dd
Daniel12346 <danezoki2@gmail.com>
2024-11-27 01:44:40 +0100

    update text label from 'lobby' to 'match' in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default async function Index() {
                 width={100}
                 height={100}
               />
-              <TextWithSlideInUnderline>lobby</TextWithSlideInUnderline>
+              <TextWithSlideInUnderline>match</TextWithSlideInUnderline>
             </Link>
           </div>
           <div className="flex h-full  flex-col gap-3 items-center group cursor-pointer">
