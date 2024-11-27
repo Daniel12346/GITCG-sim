814d3e8fd9a77a0c251d7840c9a9f4704eda2b4d
Daniel12346 <danezoki2@gmail.com>
2024-11-27 01:50:23 +0100

    updated button label from 'Redraw' to 'redraw' in CardRedraw component

--- components/CardRedraw.tsx
+++ components/CardRedraw.tsx
@@ -77,7 +77,7 @@ export default function CardRedraw({
                 setSelectedCards([]);
               }}
             >
-              Redraw
+              redraw
             </button>
             <button
               onClick={() => {
