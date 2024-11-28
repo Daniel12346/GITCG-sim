708862f0c57e182a8c9e5f9e01273b856f3a4007
Daniel12346 <danezoki2@gmail.com>
2024-11-28 18:54:45 +0100

    feat: add RequiredEnergyDisplay component

---
+++ components/RequiredEnergyDisplay.tsx
@@ -0,0 +1,24 @@
+export default function RequiredEnergyDisplay({
+  energy,
+  energySize = 6,
+}: {
+  energy: number;
+  energySize: number;
+}) {
+  return (
+    <div className="flex items-center">
+      {/* <span>energy:</span> */}
+      <ul className="flex gap-3">
+        {Array.from({
+          length: energy,
+        }).map((_, i) => (
+          <img
+            key={"energy" + i}
+            src="/energy_icon.svg"
+            className={`w-${energySize} h-${energySize}`}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
