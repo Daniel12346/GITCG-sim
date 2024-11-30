03233b5e119d1910b5aa952dad8cffe8aca5526f
Daniel12346 <danezoki2@gmail.com>
2024-11-30 01:59:30 +0100

    added components.json for shadcn/ui, updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        fieldMain: "hsl(var(--field-background-main))",
-        fieldHand: "hsl(var(--field-background-hand))",
-        fieldSecondary: "hsl(var(--field-background-secondary))",
-        btn: {
-          background: "hsl(var(--btn-background))",
-          "background-hover": "hsl(var(--btn-background-hover))",
-        },
-        overlay: "hsl(var(--overlay-background))",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			fieldMain: 'hsl(var(--field-background-main))',
+  			fieldHand: 'hsl(var(--field-background-hand))',
+  			fieldSecondary: 'hsl(var(--field-background-secondary))',
+  			btn: {
+  				background: 'hsl(var(--btn-background))',
+  				'background-hover': 'hsl(var(--btn-background-hover))'
+  			},
+  			overlay: 'hsl(var(--overlay-background))'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
