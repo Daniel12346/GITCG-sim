3ff8e70715e6c841d021c610c15526ade1b29935
Daniel12346 <danezoki2@gmail.com>
2024-12-03 01:43:00 +0100

    feat: reset opponent ID and game ID on component unmount in GameBoard

--- components/GameBoard.tsx
+++ components/GameBoard.tsx
@@ -1,12 +1,20 @@
-import { myIDState, opponentIDState } from "@/recoil/atoms";
-import { useRecoilValue } from "recoil";
+import { myIDState, opponentIDState, currentGameIDState } from "@/recoil/atoms";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import PlayerBoard from "./PlayerBoard";
 import TurnAndPhase from "./TurnAndPhase";
+import { useEffect } from "react";
 
 export default function GameBoard() {
   const myID = useRecoilValue(myIDState);
-  const opponentID = useRecoilValue(opponentIDState);
+  const [opponentID, setOpponentID] = useRecoilState(opponentIDState);
+  const setGameID = useSetRecoilState(currentGameIDState);
 
+  useEffect(() => {
+    return () => {
+      setOpponentID("");
+      setGameID("");
+    };
+  }, []);
   return (
     <div className="w-full">
       <PlayerBoard playerID={opponentID} />
