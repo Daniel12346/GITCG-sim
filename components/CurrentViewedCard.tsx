fa3fdc9a935e1f17a1dd2be95dd4bb0012d6a0e2
Daniel12346 <danezoki2@gmail.com>
2024-12-07 00:34:06 +0100

    feat: add counters display to CurrentViewedCard component

--- components/CurrentViewedCard.tsx
+++ components/CurrentViewedCard.tsx
@@ -24,81 +24,105 @@ export default () => {
             )}
           </div>
         )}
-        {/* statuses are only displayed in game */}
-        {currentGameID && currentCard?.statuses?.length !== 0 && (
-          <div className="flex flex-col w-full p-2 gap-1">
-            <span>STATUSES</span>
-            {currentCard?.statuses?.map((status) => (
-              <div className="flex items-center gap-1">
-                <span>
-                  <img
-                    className="w-6 h-6"
-                    src={`/${status.name.toLowerCase()}.svg`}
-                  ></img>
-                </span>
-                <span
-                  //TODO: use a unique key
-                  key={status.name}
-                >{`${status.name.replace("_", " ")}  ${
-                  status.turnsLeft ?? ""
-                }`}</span>
+        {currentCard && (
+          <>
+            {/* statuses are only displayed in game */}
+            {currentGameID && currentCard?.statuses?.length !== 0 && (
+              <div className="flex flex-col w-full p-2 gap-1">
+                <span>STATUSES</span>
+                {currentCard?.statuses?.map((status) => (
+                  <div className="flex items-center gap-1">
+                    <span>
+                      <img
+                        className="w-6 h-6"
+                        src={`/${status.name.toLowerCase()}.svg`}
+                      ></img>
+                    </span>
+                    <span
+                      //TODO: use a unique key
+                      key={status.name}
+                    >{`${status.name.replace("_", " ")}  ${
+                      status.turnsLeft ?? ""
+                    }`}</span>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        )}
-        <div className="flex flex-col gap-1  max-h-[10rem] overflow-y-scroll translate-x-0">
-          {currentCard?.effects &&
-            currentCard.effects
-              ?.toSorted((a: Effect, b: Effect) => {
-                const order = [
-                  "NORMAL_ATTACK",
-                  "ELEMENTAL_SKILL",
-                  "ELEMENTAL_BURST",
-                ];
-                return (
-                  order.indexOf(a.effectType!) - order.indexOf(b.effectType!)
-                );
-              })
-              .map((effect) => {
-                return (
-                  <div className="w-full p-2" key={effect.id}>
-                    {(effect.effectType === "ELEMENTAL_BURST" ||
-                      effect.effectType === "ELEMENTAL_SKILL" ||
-                      effect.effectType === "NORMAL_ATTACK") && (
-                      <span className="text-blue-200/90">
-                        {effect.effectType.replace(/_/g, " ")}
-                      </span>
-                    )}
-                    <p className="w-full text-slate-200">
-                      {effect.description}
-                    </p>
-                    <div className="flex gap-[0.5rem]">
-                      {effect.cost && calculateTotalDice(effect.cost) !== 0 && (
-                        <div>
-                          <DiceDisplay
-                            dice={Object.fromEntries(
-                              Object.entries(effect.cost).filter(
-                                ([key]) => key != "ENERGY"
-                              )
-                            )}
-                            isMyBoard={false}
-                          />
-                          {effect.effectType === "ELEMENTAL_BURST" &&
-                            currentCard.max_energy && (
-                              <div className="px-4">
-                                <RequiredEnergyDisplay
-                                  energy={currentCard.max_energy}
-                                  energySize={4}
+            )}
+            {currentGameID && currentCard?.counters?.length !== 0 && (
+              <div className="flex flex-col w-full p-2 gap-1">
+                <span>COUNTERS</span>
+                {currentCard?.counters?.map((counter) => (
+                  <div className="flex items-center gap-1">
+                    <span
+                      //TODO: use a unique key
+                      key={counter.name}
+                    >{`${counter.name.replace("_", " ")}  ${
+                      counter.amount ?? ""
+                    }`}</span>
+                  </div>
+                ))}
+              </div>
+            )}
+
+            {/* //TODO: counter name, multiple countes */}
+
+            <div className="flex flex-col gap-1  max-h-[10rem] overflow-y-scroll translate-x-0">
+              {currentCard?.effects &&
+                currentCard.effects
+                  ?.toSorted((a: Effect, b: Effect) => {
+                    const order = [
+                      "NORMAL_ATTACK",
+                      "ELEMENTAL_SKILL",
+                      "ELEMENTAL_BURST",
+                    ];
+                    return (
+                      order.indexOf(a.effectType!) -
+                      order.indexOf(b.effectType!)
+                    );
+                  })
+                  .map((effect) => {
+                    return (
+                      <div className="w-full p-2" key={effect.id}>
+                        {(effect.effectType === "ELEMENTAL_BURST" ||
+                          effect.effectType === "ELEMENTAL_SKILL" ||
+                          effect.effectType === "NORMAL_ATTACK") && (
+                          <span className="text-blue-200/90">
+                            {effect.effectType.replace(/_/g, " ")}
+                          </span>
+                        )}
+                        <p className="w-full text-slate-200">
+                          {effect.description}
+                        </p>
+                        <div className="flex gap-[0.5rem]">
+                          {effect.cost &&
+                            calculateTotalDice(effect.cost) !== 0 && (
+                              <div>
+                                <DiceDisplay
+                                  dice={Object.fromEntries(
+                                    Object.entries(effect.cost).filter(
+                                      ([key]) => key != "ENERGY"
+                                    )
+                                  )}
+                                  isMyBoard={false}
                                 />
+                                {effect.effectType === "ELEMENTAL_BURST" &&
+                                  currentCard.max_energy && (
+                                    <div className="px-4">
+                                      <RequiredEnergyDisplay
+                                        energy={currentCard.max_energy}
+                                        energySize={4}
+                                      />
+                                    </div>
+                                  )}
                               </div>
                             )}
                         </div>
-                      )}
-                    </div>
-                  </div>
-                );
-              })}
-        </div>
+                      </div>
+                    );
+                  })}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
