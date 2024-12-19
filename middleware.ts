62d5cc9006f51872c47370f2218d34eaff4d48a8
Daniel12346 <danezoki2@gmail.com>
2024-12-19 08:25:10 +0100

    feat: implement session management in middleware and redirect unauthenticated users to login

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,34 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
 
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
+  const res = NextResponse.next();
 
   // Create a Supabase client configured to use cookies
-  const supabase = createMiddlewareClient({ req, res })
+  const supabase = createMiddlewareClient({ req, res });
 
   // Refresh session if expired - required for Server Components
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-  await supabase.auth.getSession()
-
-  return res
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  // Redirect to login if no session (unauthenticated)
+  if (!session && req.url !== "/login") {
+    return NextResponse.rewrite(new URL("/login", req.url));
+  }
+  return res;
 }
+
+export const config = {
+  matcher: [
+    "/",
+    "/me",
+    "/player/:path*",
+    "/lobby",
+    "/battle-log",
+    "/deck-builder",
+    "/game/:path*",
+  ],
+};
