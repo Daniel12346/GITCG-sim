137182bcef30d8513f22318338cfeeb0042a218e
Daniel12346 <danezoki2@gmail.com>
2025-11-26 23:12:28 +0100

    used signOutAction

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,6 +1,8 @@
+import { signOutAction } from "@/app/actions";
+
 export default function LogoutButton() {
   return (
-    <form action="/auth/sign-out" method="post">
+    <form action={signOutAction} method="post">
       <button className="py-1 px-2 rounded no-underline text-slate-200 bg-blue-800 hover:bg-blue-700">
         Logout
       </button>
