d39916d4fe524e15fb6477d1b198a95e1582b87d
Daniel12346 <danezoki2@gmail.com>
2025-11-26 23:57:20 +0100

    refactor LogoutButton to reset deck ID on logout

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,9 +1,20 @@
+"use client";
 import { signOutAction } from "@/app/actions";
+import { myCurrentDeckIDState } from "@/recoil/atoms";
+import { useResetRecoilState } from "recoil";
 
 export default function LogoutButton() {
+  const resetMyCurrentDeckID = useResetRecoilState(myCurrentDeckIDState);
   return (
-    <form action={signOutAction} method="post">
-      <button className="py-1 px-2 rounded no-underline text-slate-200 bg-blue-800 hover:bg-blue-700">
+    <form action={signOutAction}>
+      <button
+        className="py-1 px-2 rounded no-underline text-slate-200 bg-blue-800 hover:bg-blue-700 "
+        onClick={(e) => {
+          e.preventDefault();
+          resetMyCurrentDeckID();
+          e.currentTarget.form?.requestSubmit();
+        }}
+      >
         Logout
       </button>
     </form>
