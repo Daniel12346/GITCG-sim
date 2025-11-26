69374e2e5bd6227e059d1e7f43d019b2a9d26e34
Daniel12346 <danezoki2@gmail.com>
2025-11-26 19:44:28 +0100

    refactor middleware to use updateSession for session management

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,17 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
-
-import type { NextRequest } from "next/server";
-
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-
-  // Create a Supabase client configured to use cookies
-  const supabase = createMiddlewareClient({ req, res });
-
-  // Refresh session if expired - required for Server Components
-  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-  // Redirect to login if no session (unauthenticated)
-  if (!session && req.url !== "/login") {
-    return NextResponse.rewrite(new URL("/login", req.url));
-  }
-  return res;
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
 }
-
 export const config = {
   matcher: [
-    "/",
-    "/me",
-    "/player/:path*",
-    "/lobby",
-    "/battle-log",
-    "/deck-builder",
-    "/game/:path*",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
