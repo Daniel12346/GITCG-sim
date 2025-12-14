2f6cae5d2f6d6c27b292124cb9971804b1019327
Daniel12346 <danezoki2@gmail.com>
2025-12-14 21:32:14 +0100

    Add min-h-screen class to lobby container for full height

--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -10,7 +10,7 @@ export default function Lobby() {
   });
 
   return (
-    <div className="pt-20 max-w-xl w-full">
+    <div className="pt-20 max-w-xl w-full min-h-screen">
       <Link href="/">
         <button className="text-lg flex items-center text-slate-100  mb-4 text-center rounded px-2 py-1 bg-red-800 hover:bg-red-700">
           <ChevronLeft size={24} />
